43c11e39372e5beea234427b0b3a3d6f29979100
Ailurus2233 <Ailurus2233@outlook.com>
2021-09-18 16:54:08 +0800

    init git repo

---
+++ 3/d/d.cpp
@@ -0,0 +1,21 @@
+/* Problem: 
+ * Author: ailurus2233
+ * Time: 2021-09-18 15:44:47
+**/
+
+#include <bits/stdc++.h>
+
+/* define block */
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    /* Code Block */
+
+    //please AC!!
+    return 0;
+}
---
+++ templete.cpp
@@ -0,0 +1,21 @@
+/* Problem: 
+ * Author: $%U%$
+ * Time: $%Y%$-$%M%$-$%D%$ $%h%$:$%m%$:$%s%$
+**/
+
+#include <bits/stdc++.h>
+
+/* define block */
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    /* Code Block */
+
+    //please AC!!
+    return 0;
+}
