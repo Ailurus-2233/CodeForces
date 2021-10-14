ba14555a9a930586782007a96562265e6a340e59
wzy <ailurus2233@outlook.com>
2021-10-14 13:25:23 +0800

    add contest 4

---
+++ contest/4/a/a.cpp
@@ -0,0 +1,21 @@
+/* Problem: 
+ * Author: ailurus2233
+ * Time: 2021-10-14 13:23:28
+**/
+
+#include <bits/stdc++.h>
+
+/* define block */
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    /* Code Block */
+
+    //please AC!!
+    return 0;
+}
